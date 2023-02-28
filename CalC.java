d718914e07f2c2ce1672925345c35a740da92b1e
Dhruv Gupta <dhruv.sgit @gmail.com>
2023-02-28 19:41:50 +0530

    First commit for branch2

--- CalC.java
+++ CalC.java
@@ -2,5 +2,6 @@ public class HelloWorld{
    public static void main(String [] args){
         System.out.println("This is hello world class");
         //Comment added
+        //Comment from branch2
    }
 } 
